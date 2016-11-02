8f22343f6d8ea78836183de2f461b3e28357acf3
Matt Vickery <matt.vickery@groupcdg.com>
2016-11-02 19:23:49 +0000

    #5 dependency names corrected after renaming.

--- greendot-db-jpa-core/src/main/java/com/greendot/db/jpa/configuration/PropertiesConfiguration.java
+++ greendot-db-jpa-core/src/main/java/com/greendot/db/jpa/configuration/PropertiesConfiguration.java
@@ -3,21 +3,6 @@
  *  which is part of this source code package.
  */
 
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
 package com.greendot.db.jpa.configuration;
 
 import org.springframework.context.annotation.Bean;
