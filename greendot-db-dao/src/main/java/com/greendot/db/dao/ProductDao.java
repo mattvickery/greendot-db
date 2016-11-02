87e232343268242be644085c4a7a92565ea70d1e
Matt Vickery <matt.d.vickery@gmail.com>
2016-11-02 18:47:39 +0000

    Remove duplicate copyright statements.

--- greendot-db-dao/src/main/java/com/greendot/db/dao/ProductDao.java
+++ greendot-db-dao/src/main/java/com/greendot/db/dao/ProductDao.java
@@ -3,26 +3,6 @@
  *  which is part of this source code package.
  */
 
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
 package com.greendot.db.dao;
 
 import com.greendot.db.jpa.core.MutatingEntityDao;
