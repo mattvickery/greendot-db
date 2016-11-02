19a7fc0fb905f6a9fca48292dca5b3762ac2a8fc
Matt Vickery <matt.d.vickery@gmail.com>
2016-11-02 19:27:25 +0000

    Remove duplicate copyright statements.

--- greendot-db-jpa-core/src/main/java/com/greendot/db/jpa/configuration/JpaDatabaseConfiguration.java
+++ greendot-db-jpa-core/src/main/java/com/greendot/db/jpa/configuration/JpaDatabaseConfiguration.java
@@ -3,26 +3,6 @@
  *  which is part of this source code package.
  */
 
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
-/*
- *  This file is subject to the terms and conditions defined in file 'LICENSE.md',
- *  which is part of this source code package.
- */
-
 package com.greendot.db.jpa.configuration;
 
 import org.apache.commons.dbcp2.BasicDataSource;
